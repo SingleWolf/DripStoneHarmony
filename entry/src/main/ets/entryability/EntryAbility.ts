79ffb712c7f48252d9fac44424ac89b75af5280f
Walker <feitianwumu@163.com>
2024-03-07 23:12:27 +0800

    添加工具类

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,19 +1,29 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import EntryContext from '../common/utils/EntryContext';
+import WindowUtils from '../common/utils/WindowUtils';
 
 export default class EntryAbility extends UIAbility {
+
+  tag: string = "EntryAbility"
+
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    EntryContext.setContext(this.context)
   }
 
   onDestroy() {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
-  onWindowStageCreate(windowStage: window.WindowStage) {
+  async onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+    let windowClass = await windowStage.getMainWindow();
+    hilog.info(0x0000, this.tag, 'Succeeded in obtaining the main window. Data: ' + JSON.stringify(windowClass));
+    WindowUtils.init(windowClass);
 
     windowStage.loadContent('pages/Splash', (err, data) => {
       if (err.code) {
